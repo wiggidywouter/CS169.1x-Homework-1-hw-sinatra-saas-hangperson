7fe505f9c685394e034aa4210388cf646a74a425
wiggidywouter <wiggidywouter@yandex.com>
2015-11-09 17:49:49 +0100

    hangman app fix 4

--- app.rb
+++ app.rb
@@ -40,9 +40,9 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
-    if letter.nil? or /[^a-z]/i === letter
+    if letter.nil? || /[^a-z]/i === letter
       flash[:message] = 'Invalid guess.'
-    if @game.guesses.include?(letter) || @game.wrong_guesses.include?(letter)
+    elsif @game.guesses.include?(letter) || @game.wrong_guesses.include?(letter)
       flash[:message] = 'You have already used that letter.'
     else
       @game.guess(letter)
